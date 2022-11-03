d333e52797f056e353aa8769c9ce14d6c2e658bd
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 16:44:56 +0900

    Create chap03_challengeQ04.sql

---
+++ Oracle/Chapter03/chap03_challengeQ04.sql
@@ -0,0 +1,4 @@
+SELECT      last_name, 
+            ROUND(MONTHS_BETWEEN(SYSDATE, hire_date)) AS "MONTHS_WORKED" 
+FROM        hr.employees 
+ORDER BY    ROUND(MONTHS_BETWEEN(SYSDATE, hire_date)) ASC
