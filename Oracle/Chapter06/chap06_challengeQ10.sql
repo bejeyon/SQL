fce0534fa7b04faaf3b156f78ba8710d4e4d9183
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 21:54:05 +0900

    Update chap06_challengeQ10.sql

--- Oracle/Chapter06/chap06_challengeQ10.sql
+++ Oracle/Chapter06/chap06_challengeQ10.sql
@@ -1,7 +1,8 @@
-SELECT          e.job_id,
-                COUNT(e.employee_id)
-FROM            hr.departments d
-INNER JOIN      hr.employees e
-ON              d.department_id = e.department_id
-WHERE           d.department_name IN ('Administration', 'Executive')
-GROUP BY        e.job_id;
+SELECT          e.job_id, 
+                COUNT(e.employee_id) 
+FROM            hr.departments d 
+INNER JOIN      hr.employees e 
+ON              d.department_id = e.department_id 
+WHERE           d.department_name IN ('Administration', 'Executive') 
+GROUP BY        e.job_id
+ORDER BY        2 DESC;
