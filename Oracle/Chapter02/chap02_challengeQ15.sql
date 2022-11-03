3ccae38df282eb2dda3fd2cf65d80f4627e2a893
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 16:40:00 +0900

    Create chap02_challengeQ15.sql

---
+++ Oracle/Chapter02/chap02_challengeQ15.sql
@@ -0,0 +1,7 @@
+SELECT      last_name, 
+            job_id, 
+            salary, 
+            commission_pct 
+FROM        hr.employees 
+WHERE       NOT(commission_pct IS NULL) 
+ORDER BY    salary DESC
