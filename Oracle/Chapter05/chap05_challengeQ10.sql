e0b5cce2020eb445fb2006f2fdde8064d3a6d4f6
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 16:59:52 +0900

    Create chap05_challengeQ10.sql

---
+++ Oracle/Chapter05/chap05_challengeQ10.sql
@@ -0,0 +1,8 @@
+SELECT      COUNT(*), 
+            SUM(CASE    TO_CHAR(hire_date, 'YYYY') 
+                        WHEN    '2005'  THEN    1 
+                END)    AS  "2005", 
+            SUM(CASE    TO_CHAR(hire_date, 'YYYY') 
+                        WHEN    '2006'  THEN    1 
+                END)    AS  "2005" 
+FROM        hr.employees
