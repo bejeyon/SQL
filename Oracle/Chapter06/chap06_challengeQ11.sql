eb4d69178de8c81093dafe839d66f5eb277ae4b2
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 21:22:15 +0900

    Create chap06_challengeQ11.sql

---
+++ Oracle/Chapter06/chap06_challengeQ11.sql
@@ -0,0 +1,8 @@
+SELECT          d.department_id,
+                d.department_name,
+                COUNT(e.employee_id),
+                ROUND(AVG(e.salary))
+FROM            hr.departments d
+INNER JOIN      hr.employees e
+ON              d.department_id = e.department_id
+GROUP BY        d.department_id, d.department_name;
