2c1081351873d9e799356061ef38619bd4a48532
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 20:57:01 +0900

    Create chap06_challengeQ06.sql

---
+++ Oracle/Chapter06/chap06_challengeQ06.sql
@@ -0,0 +1,6 @@
+SELECT          e1.last_name AS "LAST_NAME" ,
+                e1.hire_date AS "HIRE_DATE"
+FROM            hr.employees e1
+INNER JOIN      hr.employees e2
+ON              e1.hire_date > e2.hire_date
+WHERE           e2.last_name = 'Davies';
