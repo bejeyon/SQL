bf94652c9bbb59c68becb074aa5e63a7234c371d
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 17:00:12 +0900

    Create chap05_challengeQ11.sql

---
+++ Oracle/Chapter05/chap05_challengeQ11.sql
@@ -0,0 +1,10 @@
+SELECT      job_id  AS  "Job", 
+            SUM(CASE    department_id 
+                        WHEN    20  THEN    salary 
+                END)    AS "Dept 20", 
+            SUM(CASE    department_id 
+                        WHEN    50  THEN    salary 
+                END)    AS "Dept 50", 
+            SUM(salary)    AS "Total" 
+FROM        hr.employees 
+GROUP BY    job_id
