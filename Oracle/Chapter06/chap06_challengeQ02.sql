66de92547ffd1fe07b564d88c8d5cbfbd6966be1
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 20:23:53 +0900

    Create chap06_challengeQ02.sql

---
+++ Oracle/Chapter06/chap06_challengeQ02.sql
@@ -0,0 +1,6 @@
+SELECT          last_name,
+                e.department_id,
+                department_name
+FROM            hr.employees e
+INNER JOIN      hr.departments d
+on              (e.department_id = d.department_id);
