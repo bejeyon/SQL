392c9383545c4ff104025c46bee36f3bfc517f3e
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 16:51:30 +0900

    Create chap04_challengeQ04.sql

---
+++ Oracle/Chapter04/chap04_challengeQ04.sql
@@ -0,0 +1,6 @@
+SELECT      last_name, 
+(CASE       TO_CHAR(NVL(commission_pct, 0)) 
+            WHEN    '0'   THEN  'No Commission' 
+            ELSE                TO_CHAR(NVL(commission_pct, 0)) 
+END)        AS "COMM" 
+FROM        hr.employees
