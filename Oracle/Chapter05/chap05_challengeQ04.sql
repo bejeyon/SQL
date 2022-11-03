6f7b62768f9fe2505024ffd2e0cc90846fd71146
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 16:56:54 +0900

    Create chap05_challengeQ04.sql

---
+++ Oracle/Chapter05/chap05_challengeQ04.sql
@@ -0,0 +1,5 @@
+SELECT      ROUND(MAX(salary)) AS "Maximum", 
+            ROUND(MIN(salary)) AS "Minimum", 
+            ROUND(SUM(salary)) AS "Sum", 
+            ROUND(AVG(salary)) AS "Average" 
+FROM        hr.employees
