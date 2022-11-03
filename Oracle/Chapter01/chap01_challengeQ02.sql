3a0488611b722e697a30c969a90275b76b616486
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-03 16:25:59 +0900

    Update chap01_challengeQ02.sql

--- Oracle/Chapter01/chap01_challengeQ02.sql
+++ Oracle/Chapter01/chap01_challengeQ02.sql
@@ -1,2 +1,5 @@
-SELECT employee_id, last_name, job_id, (hire_date) as StartDate 
-FROM hr.employees
+SELECT  employee_id, 
+        last_name,  
+        job_id,  
+        (hire_date) as StartDate 
+FROM    hr.employees
