c492288668507adf729b60addfaa7b32dbb065f1
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-04 17:34:35 +0900

    Create chap07_challengeQ08_02.sql

---
+++ Oracle/Chapter07/chap07_challengeQ08_02.sql
@@ -0,0 +1,7 @@
+SELECT  department_id,
+        department_name,
+        location_id
+FROM    hr.departments
+WHERE   department_id NOT IN(SELECT NVL(department_id, 0)
+                            FROM    hr.employees
+                            WHERE   job_id = 'SA_REP');
