9fbb81e94447124c8a68d0b30995a8f7b1dd190d
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-04 16:46:53 +0900

    Create chap07_challengeQ02.sql

---
+++ Oracle/Chapter07/chap07_challengeQ02.sql
@@ -0,0 +1,7 @@
+SELECT      employee_id,
+            last_name,
+            salary
+FROM        hr.employees
+WHERE       salary > (SELECT AVG(salary)
+                        FROM    hr.employees)
+ORDER BY    3 ASC;
