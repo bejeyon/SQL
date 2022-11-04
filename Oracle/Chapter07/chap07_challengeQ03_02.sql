e6a8014a1d8daffa90a8932888245eb878712931
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-04 16:51:08 +0900

    Create chap07_challengeQ03_02.sql

---
+++ Oracle/Chapter07/chap07_challengeQ03_02.sql
@@ -0,0 +1,6 @@
+SELECT      employee_id,
+            last_name
+FROM        hr.employees
+WHERE       department_id IN(SELECT department_id
+                            FROM    hr.employees
+                            WHERE   last_name LIKE '%x%');
