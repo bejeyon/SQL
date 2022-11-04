6e10f8e02ba254f70181a4669684c780327214dc
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-04 17:10:26 +0900

    Create chap07_challengeQ05_.sql

---
+++ Oracle/Chapter07/chap07_challengeQ05_.sql
@@ -0,0 +1,8 @@
+SELECT  last_name,
+        salary
+FROM    hr.employees
+WHERE   manager_id = ANY(SELECT employee_id
+                        FROM    hr.employees
+                        WHERE   manager_id IN (SELECT   employee_id
+                                                FROM    hr.employees
+                                                WHERE   last_name = 'King'));
