18f7111dc12378379a1c09d3c40abaf0b106bb1d
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-04 17:43:10 +0900

    Create chap07_challengeQ09_02.sql
    COUNT(*)이 된다 -> 이해하기

---
+++ Oracle/Chapter07/chap07_challengeQ09_02.sql
@@ -0,0 +1,8 @@
+SELECT      d.department_id,
+            d.department_name,
+            COUNT(*)
+FROM        hr.departments d
+INNER JOIN  hr.employees e
+ON          e.department_id = d.department_id
+GROUP BY    d.department_id, d.department_name
+HAVING      COUNT(*) < 3;
