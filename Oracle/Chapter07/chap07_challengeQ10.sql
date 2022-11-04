87aa1826928d2b65b77184ba4fea6b4563a75e7c
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-04 18:12:35 +0900

    Create chap07_challengeQ10.sql

---
+++ Oracle/Chapter07/chap07_challengeQ10.sql
@@ -0,0 +1,10 @@
+SELECT      d.department_id,
+            d.department_name,
+            COUNT(*)
+FROM        hr.departments d
+INNER JOIN  hr.employees e
+ON          e.department_id = d.department_id
+GROUP BY    d.department_id, d.department_name
+HAVING      COUNT(*) = (SELECT      MAX(COUNT(*))
+                        FROM        hr.employees
+                        GROUP BY    department_id);
