1022b5ee9d9f2b3f55e95608662bfdd9416f724a
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 14:04:31 +0900

    Update chap07_challengeQ08.sql
    8. HR 부서에서 직무[job_id]가 영업 사원[SA_REP]인 직원이 근무하지 않는 부서를 표시하는 보고서를 작성합니다. 출력에 부서 번호[department_id], 부서 이름[department_name] 및 위치[location_id]를 포함합니다.

--- Oracle/Chapter07/chap07_challengeQ08.sql
+++ Oracle/Chapter07/chap07_challengeQ08.sql
@@ -4,4 +4,4 @@ SELECT  department_id,
 FROM    hr.departments
 WHERE   department_id <> ALL(SELECT NVL(department_id, 0)
                             FROM    hr.employees
-                            WHERE   job_id = 'SA_REP')
+                            WHERE   job_id = 'SA_REP');
