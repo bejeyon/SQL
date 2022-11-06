e8e5d84ce5f5e950e5cade84d9ebeefa794956cf
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 14:07:05 +0900

    Update chap07_challengeQ11.sql
    11. HR 부서를 위해 통계 보고서를 작성합니다. 평균 급여[salary]가 가장 높은 부서의 부서 번호[department_id]와 평균 급여[salary]를 표시하는 보고서를 작성합니다. 평균 급여[salary] 출력 시 정수 반올림처리
    힌트: GROUP BY , HAVING, 그룹 함수 중첩

--- Oracle/Chapter07/chap07_challengeQ11.sql
+++ Oracle/Chapter07/chap07_challengeQ11.sql
@@ -4,4 +4,4 @@ FROM        hr.employees
 GROUP BY    department_id
 HAVING      AVG(salary) = (SELECT       MAX(AVG(salary))
                             FROM        hr.employees
-                            GROUP BY    department_id)
+                            GROUP BY    department_id);
