10901d1ac2f0e436f61136965d02a1c909d3d8c9
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:55:22 +0900

    Update chap06_challengeQ06.sql

--- Oracle/Chapter06/chap06_challengeQ06.sql
+++ Oracle/Chapter06/chap06_challengeQ06.sql
@@ -3,4 +3,4 @@ SELECT          e1.last_name AS "LAST_NAME" ,
 FROM            hr.employees e1
 INNER JOIN      hr.employees e2
 ON              e1.hire_date > e2.hire_date
-WHERE           e2.last_name = 'Davies';
+WHERE           e2.last_name = 'Davies'
