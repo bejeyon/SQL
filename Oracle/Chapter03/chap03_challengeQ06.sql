a8b96efb88b1a69b0c8ec5efaf68e3de62ea5bb8
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:44:18 +0900

    Update chap03_challengeQ06.sql
    6. HR부서에서 사원의 성[last_name]을 표시하고 급여[salary] 액수를 별표 ( * ) 로 나타내는 query를 작성합니다. 각 별표는 $1,000 단위 당 1개로 나타냅니다. 급여[salary]의 내림차순으로 데이터를 정렬합니다. 열 alias는 EMPLOYEES_AND_THEIR_SALARIES로 지정합니다.
    [난이도 상]

--- Oracle/Chapter03/chap03_challengeQ06.sql
+++ Oracle/Chapter03/chap03_challengeQ06.sql
@@ -1,4 +1,4 @@
 SELECT      last_name, 
             TRIM(' ' FROM RPAD(' ', TRUNC(salary/1000) + 1, '*')) AS "EMPLOYEES_AND_THEIR_SALARIES" 
 FROM        hr.employees 
-ORDER BY    salary DESC
+ORDER BY    salary DESC;
