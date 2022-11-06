b784c1de377aa9150eae0c0fee698aeb9d36fe11
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 14:05:34 +0900

    Update chap07_challengeQ09.sql
    9. HR 부서를 위해 통계 보고서를 작성합니다. 이 보고서에는 다음 조건의 부서에 대한 부서 번호[department_id], 부서 이름[department_name] 및 근무하는 사원 수[]를 표시합니다. 사원 수가 3명 미만인 부서
    [난이도 상] 힌트: COUNT(*), GROUP BY , HAVING,  inner join

--- Oracle/Chapter07/chap07_challengeQ09.sql
+++ Oracle/Chapter07/chap07_challengeQ09.sql
@@ -5,4 +5,4 @@ FROM        hr.departments d
 INNER JOIN  hr.employees e
 ON          e.department_id = d.department_id
 GROUP BY    d.department_id, d.department_name
-HAVING      COUNT(e.employee_id) < 3
+HAVING      COUNT(e.employee_id) < 3;
