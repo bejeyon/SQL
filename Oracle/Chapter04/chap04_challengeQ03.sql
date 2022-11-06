628bcd56c5f44ea9a9b171d2191aee13662156de
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:46:44 +0900

    Update chap04_challengeQ03.sql
    3. HR부서에서 사원의 성[last_name], 채용 날짜[hire_date], 근무 시작 요일을 표시합니다. 열 alaias는 DAY로 지정합니다. 일요일부터 시작하여 요일순으로 결과를 정렬합니다. 참조 TO_CHAR( 날짜데이터 , 'd') 를 사용하면 요일을 숫자로 출력 가능합니다.

--- Oracle/Chapter04/chap04_challengeQ03.sql
+++ Oracle/Chapter04/chap04_challengeQ03.sql
@@ -2,4 +2,4 @@ SELECT      last_name,
             hire_date, 
             TO_CHAR(hire_date, 'DAY') 
 FROM        hr.employees 
-ORDER BY    TO_CHAR(hire_date, 'd')
+ORDER BY    TO_CHAR(hire_date, 'd');
