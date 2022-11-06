7fe0f307a4022bfcc866c33b24d168fe5264cf32
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:50:22 +0900

    Update chap05_challengeQ04.sql
    HR 부서에서 다음 보고서를 요구합니다. 
    4.  모든 사원의 최고, 최저, 합계 및 평균 급여[salary]를 찾습니다. 열 alais는 각각 Maximum, Minimum, Sum 및 Average로 지정합니다. 결과를 가장 가까운 정수로 반올림합니다. query를 작성합니다.

--- Oracle/Chapter05/chap05_challengeQ04.sql
+++ Oracle/Chapter05/chap05_challengeQ04.sql
@@ -2,4 +2,4 @@ SELECT      ROUND(MAX(salary)) AS "Maximum",
             ROUND(MIN(salary)) AS "Minimum", 
             ROUND(SUM(salary)) AS "Sum", 
             ROUND(AVG(salary)) AS "Average" 
-FROM        hr.employees
+FROM        hr.employees;
