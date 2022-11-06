a2c646ed2e2fd6e9009c463ea66c04275bc54c78
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:54:58 +0900

    Update chap06_challengeQ05.sql

--- Oracle/Chapter06/chap06_challengeQ05.sql
+++ Oracle/Chapter06/chap06_challengeQ05.sql
@@ -5,4 +5,4 @@ FROM            hr.employees EMPLOYEE
 INNER JOIN      hr.employees COLLEAGUE
 ON              EMPLOYEE.department_id = COLLEAGUE.department_id
 WHERE           EMPLOYEE.last_name <> COLLEAGUE.last_name
-ORDER BY        1 ASC, 2 ASC, 3 ASC;
+ORDER BY        1 ASC, 2 ASC, 3 ASC
