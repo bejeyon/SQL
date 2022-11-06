4e44364341d2c17ed7df288edb98da774ecc4a9c
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:58:58 +0900

    Update chap06_challengeQ11.sql

--- Oracle/Chapter06/chap06_challengeQ11.sql
+++ Oracle/Chapter06/chap06_challengeQ11.sql
@@ -5,4 +5,4 @@ SELECT          d.department_id,
 FROM            hr.departments d
 INNER JOIN      hr.employees e
 ON              d.department_id = e.department_id
-GROUP BY        d.department_id, d.department_name;
+GROUP BY        d.department_id, d.department_name
