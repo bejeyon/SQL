1f1d449ad90b250403b871685b06875baac841a9
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:54:44 +0900

    Update chap06_challengeQ04.sql
    4. 사원의 성[last_name] 및 사원 번호[employee_id]를 해당 관리자의 성[last_name] 및 관리자 번호[manager_id]와 함께 표시하는 보고서를 작성합니다. 열 alias는 각각 Employee, Emp#, Manager 및 Mgr#으로 지정합니다. 
    힌트 : self 조인 사용

--- Oracle/Chapter06/chap06_challengeQ04.sql
+++ Oracle/Chapter06/chap06_challengeQ04.sql
@@ -4,4 +4,4 @@ SELECT          worker.last_name AS "Employee" ,
                 worker.manager_id AS "Mgr#"
 FROM            hr.employees worker
 INNER JOIN      hr.employees manager
-ON              worker.manager_id = manager.employee_id
+ON              worker.manager_id = manager.employee_id;
