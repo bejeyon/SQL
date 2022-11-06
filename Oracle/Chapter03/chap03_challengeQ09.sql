eec30bcde024c79bcfa21ae7c561b5abb5c034fd
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:45:16 +0900

    Update chap03_challengeQ09.sql
    9. HR부서에서 성[last_name]이 "J", "K", "L" 또는 "M"으로 시작하는 사원의 성[last_name]을 표시합니다.

--- Oracle/Chapter03/chap03_challengeQ09.sql
+++ Oracle/Chapter03/chap03_challengeQ09.sql
@@ -1,3 +1,3 @@
 SELECT      last_name 
 FROM        hr.employees 
-WHERE       (last_name LIKE 'J%') OR (last_name LIKE 'K%') OR (last_name LIKE 'L%') OR (last_name LIKE 'M%')
+WHERE       (last_name LIKE 'J%') OR (last_name LIKE 'K%') OR (last_name LIKE 'L%') OR (last_name LIKE 'M%');
