971349b7ad6385f768511362912b5cbb1c287ba5
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:34:12 +0900

    Update chap01_challengeQ05.sql
    5. EMPLOYEES 테이블의 데이터에 익숙해지도록 해당 테이블의 모든 데이터를 표시하는 query를 생성합니다. 각 열 출력은 쉼표로 구분합니다. 열 이름을 THE_OUTPUT으로 지정합니다.

--- Oracle/Chapter01/chap01_challengeQ05.sql
+++ Oracle/Chapter01/chap01_challengeQ05.sql
@@ -1,3 +1,3 @@
 SELECT  EMPLOYEE_ID || ',' || FIRST_NAME || ',' || LAST_NAME || ',' || EMAIL || ',' || PHONE_NUMBER || ',' || JOB_ID || ',' || MANAGER_ID || ',' || HIRE_DATE || ',' || SALARY || ',' || COMMISSION_PCT || ',' || DEPARTMENT_ID 
 AS      "THE_OUTPUT" 
-FROM    hr.Employees
+FROM    hr.Employees;
