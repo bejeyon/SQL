803b5efe87d1ea36b9cde85e661f0d623418e306
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 14:03:39 +0900

    Update chap07_challengeQ07.sql

--- Oracle/Chapter07/chap07_challengeQ07.sql
+++ Oracle/Chapter07/chap07_challengeQ07.sql
@@ -2,4 +2,4 @@ SELECT  last_name
 FROM    hr.employees
 WHERE   salary > ANY(SELECT salary
                     FROM    hr.employees
-                    WHERE   department_id = 60);
+                    WHERE   department_id = 60)
