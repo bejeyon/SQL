0b09579731baec32e06ea41ee2f9393172562710
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:54:18 +0900

    Update chap06_challengeQ03.sql
    3. HR 부서에서 Toronto에 근무하는 사원에 대한 보고서를 요구합니다. Toronto에서 근무하는 모든 사원의 성[last_name], 직무[job_id], 부서 번호[department_id] 및 부서 이름[department_name]을 표시합니다.

--- Oracle/Chapter06/chap06_challengeQ03.sql
+++ Oracle/Chapter06/chap06_challengeQ03.sql
@@ -7,4 +7,4 @@ INNER JOIN      hr.departments d
 on              d.department_id = e.department_id)
 INNER JOIN      hr.LOCATIONS l
 on              l.location_id = d.location_id
-WHERE           city = 'Toronto'
+WHERE           city = 'Toronto';
