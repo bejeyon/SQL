17b535a5e8d632e5384354447eb3cd70f84a352e
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-06 13:42:36 +0900

    Update chap03_challengeQ03.sql
    3. HR 부서에서 "J", "A" 또는 "M"으로 시작하는 성[last_name]을 가진 모든 사원의 성[last_name](첫번째 문자는 대문자, 나머지는 모두 소문자)과 성[last_name]의 길이를 표시하는 query를 작성합니다. 각 열에 적절한 alias는 지정합니다. 사원의 성[last_name]을 기준으로 결과를 오름차순 정렬합니다.

--- Oracle/Chapter03/chap03_challengeQ03.sql
+++ Oracle/Chapter03/chap03_challengeQ03.sql
@@ -2,4 +2,4 @@ SELECT      last_name AS "Name",
             LENGTH(last_name) AS "Length" 
 FROM        hr.employees 
 WHERE       (last_name LIKE 'J%') OR (last_name LIKE 'A%') OR (last_name LIKE 'M%') 
-ORDER BY    last_name ASC
+ORDER BY    last_name ASC;
